14ce910cdaff81e180a88f7095c4fc38eadcf682
stevenfryto <sunzitai_1832@bupt.edu.cn>
2026-01-23 18:05:59 +0800

    implement embedding ops

---
+++ src/ops/embedding/cpu/embedding_cpu.cpp
@@ -0,0 +1,32 @@
+#include "embedding_cpu.hpp"
+
+#include "../../../utils.hpp"
+
+#include <cmath>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+
+template <typename T, typename I>
+void embedding_(T *out, const I *index, const T *weight, size_t num_indices, size_t hidden_size) {
+    for (size_t i = 0; i < num_indices; i++) {
+        memcpy(out + i * hidden_size * sizeof(T), weight + index[i] * hidden_size * sizeof(T), hidden_size * sizeof(T));
+    }
+}
+
+namespace llaisys::ops::cpu {
+void embedding(std::byte *out, const std::byte *index, const std::byte *weight, llaisysDataType_t type, size_t num_indices, size_t hidden_size) {
+    switch (type) {
+    case LLAISYS_DTYPE_F32:
+        return embedding_(reinterpret_cast<float *>(out), reinterpret_cast<const int64_t *>(index), reinterpret_cast<const float *>(weight), num_indices, hidden_size);
+    case LLAISYS_DTYPE_BF16:
+        return embedding_(reinterpret_cast<llaisys::bf16_t *>(out), reinterpret_cast<const int64_t *>(index),
+                          reinterpret_cast<const llaisys::bf16_t *>(weight), num_indices, hidden_size);
+    case LLAISYS_DTYPE_F16:
+        return embedding_(reinterpret_cast<llaisys::fp16_t *>(out), reinterpret_cast<const int64_t *>(index),
+                          reinterpret_cast<const llaisys::fp16_t *>(weight), num_indices, hidden_size);
+    default:
+        EXCEPTION_UNSUPPORTED_DATATYPE(type);
+    }
+}
+} // namespace llaisys::ops::cpu
--- src/ops/embedding/op.cpp
+++ src/ops/embedding/op.cpp
@@ -1,7 +1,33 @@
 #include "op.hpp"
 
+#include "cpu/embedding_cpu.hpp"
+#include "llaisys.h"
+
 namespace llaisys::ops {
 void embedding(tensor_t out, tensor_t index, tensor_t weight) {
-    TO_BE_IMPLEMENTED();
+    // TO_BE_IMPLEMENTED();
+    CHECK_SAME_DEVICE(out, index, weight);
+    CHECK_SAME_DTYPE(out->dtype(), weight->dtype());
+    ASSERT(out->shape()[1] == weight->shape()[1], "");
+    ASSERT(out->shape()[0] == index->shape()[0], "");
+    ASSERT(out->isContiguous() && index->isContiguous() && weight->isContiguous(), "");
+
+    if (out->deviceType() == LLAISYS_DEVICE_CPU) {
+        return cpu::embedding(out->data(), index->data(), weight->data(), out->dtype(), out->shape()[0], index->shape()[0]);
+    }
+
+    llaisys::core::context().setDevice(out->deviceType(), out->deviceId());
+
+    switch (out->deviceType()) {
+    case LLAISYS_DEVICE_CPU:
+        return cpu::embedding(out->data(), index->data(), weight->data(), out->dtype(), out->shape()[0], index->shape()[0]);
+#ifdef ENABLE_NVIDIA_API
+    case LLAISYS_DEVICE_NVIDIA:
+        TO_BE_IMPLEMENTED();
+        return;
+#endif
+    default:
+        EXCEPTION_UNSUPPORTED_DEVICE;
+    }
 }
 } // namespace llaisys::ops
