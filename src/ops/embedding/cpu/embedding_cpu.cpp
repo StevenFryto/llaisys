0c1a5b0ffe08b4788f270f07cb4cdb4bb5db49cb
stevenfreeto <sunzitai_9432@qq.com>
2026-02-02 09:27:45 +0800

    fix embedding ops

--- src/ops/embedding/cpu/embedding_cpu.cpp
+++ src/ops/embedding/cpu/embedding_cpu.cpp
@@ -10,7 +10,7 @@
 template <typename T, typename I>
 void embedding_(T *out, const I *index, const T *weight, size_t num_indices, size_t hidden_size) {
     for (size_t i = 0; i < num_indices; i++) {
-        memcpy(out + i * hidden_size * sizeof(T), weight + index[i] * hidden_size * sizeof(T), hidden_size * sizeof(T));
+        memcpy(out + i * hidden_size, weight + index[i] * hidden_size, hidden_size * sizeof(T));
     }
 }
 
--- src/ops/embedding/op.cpp
+++ src/ops/embedding/op.cpp
@@ -13,14 +13,14 @@ void embedding(tensor_t out, tensor_t index, tensor_t weight) {
     ASSERT(out->isContiguous() && index->isContiguous() && weight->isContiguous(), "");
 
     if (out->deviceType() == LLAISYS_DEVICE_CPU) {
-        return cpu::embedding(out->data(), index->data(), weight->data(), out->dtype(), out->shape()[0], index->shape()[0]);
+        return cpu::embedding(out->data(), index->data(), weight->data(), out->dtype(), out->shape()[0], weight->shape()[1]);
     }
 
     llaisys::core::context().setDevice(out->deviceType(), out->deviceId());
 
     switch (out->deviceType()) {
     case LLAISYS_DEVICE_CPU:
-        return cpu::embedding(out->data(), index->data(), weight->data(), out->dtype(), out->shape()[0], index->shape()[0]);
+        return cpu::embedding(out->data(), index->data(), weight->data(), out->dtype(), out->shape()[0], weight->shape()[1]);
 #ifdef ENABLE_NVIDIA_API
     case LLAISYS_DEVICE_NVIDIA:
         TO_BE_IMPLEMENTED();
